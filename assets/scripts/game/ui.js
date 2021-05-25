ee07170c6c4c7659756f142ca374aad6eb6652c0
MiketoCoding <le.michael.v@gmail.com>
2021-05-25 17:37:20 -0400

    Modified w/l/d conditions to make DRYer
    Updated w/l/d conditions. If 3 in a row is made, last player to make a
    move wins. .every used to shorten hard coded draw condition.

--- assets/scripts/game/ui.js
+++ assets/scripts/game/ui.js
@@ -24,49 +24,21 @@ const makeMoveSuccess = function (res) {
   store.game = res.game
   const arrayData = store.game.cells
 
-  // if (
-  //   (arrayData[0] === arrayData[1] && arrayData[0] === arrayData[2] && arrayData[0] !== '') ||
-  //   (arrayData[3] === arrayData[4] && arrayData[3] === arrayData[5] && arrayData[3] !== '') ||
-  //   (arrayData[6] === arrayData[7] && arrayData[6] === arrayData[8] && arrayData[6] !== '') ||
-  //   (arrayData[0] === arrayData[3] && arrayData[0] === arrayData[6] && arrayData[0] !== '') ||
-  //   (arrayData[1] === arrayData[4] && arrayData[1] === arrayData[7] && arrayData[1] !== '') ||
-  //   (arrayData[2] === arrayData[5] && arrayData[2] === arrayData[8] && arrayData[2] !== '') ||
-  //   (arrayData[0] === arrayData[4] && arrayData[0] === arrayData[8] && arrayData[0] !== '') ||
-  //   (arrayData[2] === arrayData[4] && arrayData[2] === arrayData[6] && arrayData[2] !== '')
-  // ) {
   if (
-    (arrayData[0] === 'X' && arrayData[1] === 'X' && arrayData[2] === 'X') ||
-      (arrayData[3] === 'X' && arrayData[4] === 'X' && arrayData[5] === 'X') ||
-      (arrayData[6] === 'X' && arrayData[7] === 'X' && arrayData[8] === 'X') ||
-      (arrayData[0] === 'X' && arrayData[3] === 'X' && arrayData[6] === 'X') ||
-      (arrayData[1] === 'X' && arrayData[4] === 'X' && arrayData[7] === 'X') ||
-      (arrayData[2] === 'X' && arrayData[5] === 'X' && arrayData[8] === 'X') ||
-      (arrayData[0] === 'X' && arrayData[4] === 'X' && arrayData[8] === 'X') ||
-      (arrayData[2] === 'X' && arrayData[4] === 'X' && arrayData[6] === 'X')
+    (arrayData[0] === arrayData[1] && arrayData[0] === arrayData[2] && arrayData[0] !== '') ||
+    (arrayData[3] === arrayData[4] && arrayData[3] === arrayData[5] && arrayData[3] !== '') ||
+    (arrayData[6] === arrayData[7] && arrayData[6] === arrayData[8] && arrayData[6] !== '') ||
+    (arrayData[0] === arrayData[3] && arrayData[0] === arrayData[6] && arrayData[0] !== '') ||
+    (arrayData[1] === arrayData[4] && arrayData[1] === arrayData[7] && arrayData[1] !== '') ||
+    (arrayData[2] === arrayData[5] && arrayData[2] === arrayData[8] && arrayData[2] !== '') ||
+    (arrayData[0] === arrayData[4] && arrayData[0] === arrayData[8] && arrayData[0] !== '') ||
+    (arrayData[2] === arrayData[4] && arrayData[2] === arrayData[6] && arrayData[2] !== '')
   ) {
     store.gameOver = true
-    console.log('Game status, is game over?', store.gameOver)
-    // this is broken, you can't click after game is over and in new game
-    // $('.box').off('click')
-    // display who won
-    $('#messaging').text('Game Over, Player X wins. That\'s you!')
-    // else if for a draw
-  } else if (
-    (arrayData[0] === 'O' && arrayData[1] === 'O' && arrayData[2] === 'O') ||
-    (arrayData[3] === 'O' && arrayData[4] === 'O' && arrayData[5] === 'O') ||
-    (arrayData[6] === 'O' && arrayData[7] === 'O' && arrayData[8] === 'O') ||
-    (arrayData[0] === 'O' && arrayData[3] === 'O' && arrayData[6] === 'O') ||
-    (arrayData[1] === 'O' && arrayData[4] === 'O' && arrayData[7] === 'O') ||
-    (arrayData[2] === 'O' && arrayData[5] === 'O' && arrayData[8] === 'O') ||
-    (arrayData[0] === 'O' && arrayData[4] === 'O' && arrayData[8] === 'O') ||
-    (arrayData[2] === 'O' && arrayData[4] === 'O' && arrayData[6] === 'O')
-  ) {
-    store.gameOver = true
-    $('#messaging').text('Game Over, You lose! Player O wins. Try your skill again!')
-  } else if (arrayData[0] !== '' && arrayData[1] !== '' && arrayData[2] !== '' && arrayData[3] !== '' && arrayData[4] !== '' && arrayData[5] !== '' && arrayData[6] !== '' && arrayData[7] !== '' && arrayData[8] !== '') {
+    $('#messaging').text(`Game Over, player ${store.currentPlayer} wins! Try your skills again!`)
+  } else if (arrayData.every(element => element !== '')) {
     store.gameOver = true
     console.log('Game status, is game over?', store.gameOver)
-    // $('.box').off('click')
     $('#messaging').text('Game Over, it\'s a draw')
   }
   store.currentPlayer = store.currentPlayer === 'O' ? 'X' : 'O'
